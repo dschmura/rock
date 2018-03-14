db318917d050f8dda0fead60bafeb1ebf16b4911
David Chmura <dschmura@humbledaisy.com>
2018-03-14 16:40:00 -0400

    UPDATE: configs for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,117 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+  set :application,     "rock"
+  set :repo_url,        "git@github.com:dschmura/rock.git"
+  set :user,            'deployer'
+  set :puma_threads,    [4, 16]
+  set :puma_workers,    0
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+  # Don't change these unless you know what you're doin
+  set :pty,             true
+  set :use_sudo,        false
+  set :stage,           :production
+  set :deploy_via,      :remote_cache
+  set :deploy_to,       "/home/#{fetch(:user)}/apps/rock"
+  set :puma_bind,       "unix://#{shared_path}/tmp/sockets/rock-puma.sock"
+  set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+  set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+  set :puma_access_log, "#{release_path}/log/puma.error.log"
+  set :puma_error_log,  "#{release_path}/log/puma.access.log"
+  set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+  set :puma_preload_app, false
+  set :puma_worker_timeout, nil
+  set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+  set :nginx_sites_enabled_path, "/etc/nginx/sites-enabled"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+  namespace :puma do
+    desc 'Create Directories for Puma Pids and Socket'
+    task :make_dirs do
+      on roles(:app) do
+        execute "mkdir #{shared_path}/tmp/sockets -p"
+        execute "mkdir #{shared_path}/tmp/pids -p"
+      end
+    end
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+      before :start, :make_dirs
+  end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+  namespace :deploy do
 
-# Default value for :pty is false
-# set :pty, true
+    desc "Make sure local git is in sync with remote."
+    task :check_revision do
+      on roles(:app) do
+        unless `git rev-parse HEAD` == `git rev-parse origin/master`
+          puts "WARNING: HEAD is not the same as origin/master"
+          puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
+    desc 'Initial Deploy'
+      task :initial do
+      on roles(:app) do
+        before 'deploy:restart', 'puma:start'
+        invoke 'deploy'
+      end
+    end
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+    desc 'Run rake yarn:install'
+    task :yarn_install do
+      on roles(:web) do
+        within release_path do
+          execute("cd #{release_path} && yarn install")
+        end
+      end
+    end
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+    # desc 'Configure nginx on server'
+    # task :nginx_install do
+    #   on roles (:app) do
+    #     within '/etc/nginx/sites-enabled/' do
+    #       execute("ln -nfs /home/deployer/apps/rock/current/config/nginx.conf /etc/nginx/sites-enabled/rock && rm /etc/nginx/sites-enabled/default")
+    #     end
+    #   end
+    # end
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
+    desc 'Restart application'
+      task :restart do
+        on roles(:app), in: :sequence, wait: 5 do
+        invoke 'puma:restart'
+      end
+    end
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+    desc 'Upload to shared/config'
+      task :upload do
+      on roles (:app) do
+        upload! "config/master.key",  "#{shared_path}/config/master.key"
+        upload! "config/puma.sample.rb",  "#{shared_path}/config/puma.rb"
+        upload! "config/nginx.conf",  "#{shared_path}/config/nginx.conf"
+      end
+    end
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  before :starting,  :check_revision
+  # before "deploy:assets:precompile", "deploy:yarn_install"
+  # after  :finishing, :compile_assets
+  after  :finishing, :cleanup
+
+  end
+
+  desc "Run rake db:seed on a remote server."
+  task :seed do
+    on roles (:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+  # ps aux | grep puma    # Get puma pid
+  # kill -s SIGUSR2 pid   # Restart puma
+  # kill -s SIGTERM pid   # Stop puma
+  ## Linked Files & Directories (Default None):
+
+  set :linked_files, %w{config/puma.rb config/master.key}
+  set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+  set :linked_dirs, fetch(:linked_dirs, []).push('public/packs', 'node_modules')
