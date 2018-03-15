741d8e08d7fe7f7804767b44e650de4761fa8a5e
David Chmura <dschmura@humbledaisy.com>
2018-03-15 15:04:02 -0400

    UPDATE remove nonsence from production.js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,15 +1,3 @@
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
-
-plugins: [
-  new webpack.LoaderOptionsPlugin({
-    minimize: true
-  }),
-  new webpack.optimize.UglifyJsPlugin(),
-  new CompressionPlugin({
-    asset: '[path].gz[query]',
-    algorithm: 'gzip',
-    test: /\.js$/
-  })
-]
